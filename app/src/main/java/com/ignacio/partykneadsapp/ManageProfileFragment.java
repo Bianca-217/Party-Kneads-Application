0439b89587bf03ce72ab32a2871956242654bce8
Bianca-217 <sweetkatrinabiancaignacio@gmail.com>
2024-11-22 01:15:53 +0800

    manage profile end icon removed

--- app/src/main/java/com/ignacio/partykneadsapp/ManageProfileFragment.java
+++ app/src/main/java/com/ignacio/partykneadsapp/ManageProfileFragment.java
@@ -1,9 +1,12 @@
 package com.ignacio.partykneadsapp;
 
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +14,7 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,6 +28,7 @@ public class ManageProfileFragment extends Fragment {
 
     private FragmentManageProfileBinding binding;
     private FirebaseFirestore firestore;
+    private ConstraintLayout cl;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,11 +48,13 @@ public class ManageProfileFragment extends Fragment {
         return binding.getRoot();
     }
 
-
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+        cl = view.findViewById(R.id.clayout);
+        cl.setOnClickListener(v -> hideKeyboard(v));
 
 
         // Back button listener
@@ -61,6 +68,13 @@ public class ManageProfileFragment extends Fragment {
             dialogFragment.show(getParentFragmentManager(), "ChangePasswordDialog");
         });
 
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) view.getContext().getSystemService(INPUT_METHOD_SERVICE);
+        if (inputMethodManager != null) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
 
