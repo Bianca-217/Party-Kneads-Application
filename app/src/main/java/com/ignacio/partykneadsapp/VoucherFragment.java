21306896558c55e3d3bef2ea50d43e334317f4ab
Bianca-217 <sweetkatrinabiancaignacio@gmail.com>
2024-11-29 08:51:06 +0800

    cancelled items fragment - customer side

---
+++ app/src/main/java/com/ignacio/partykneadsapp/VoucherFragment.java
@@ -0,0 +1,25 @@
+package com.ignacio.partykneadsapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ignacio.partykneadsapp.databinding.FragmentVoucherBinding;
+
+
+public class VoucherFragment extends Fragment {
+
+   private FragmentVoucherBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentVoucherBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+}
