aa38989e64de19fe2b658ce8bed95e21b18731b2
vz-dm <zionmingi123@gmail.com>
2024-11-29 19:22:35 +0800

    popular homepage.

--- app/src/main/java/com/ignacio/partykneadsapp/model/PopularModel.java
+++ app/src/main/java/com/ignacio/partykneadsapp/model/PopularModel.java
@@ -1,37 +1,32 @@
 package com.ignacio.partykneadsapp.model;
 
+
 public class PopularModel {
     private String name;
-    private String rate;
     private String sold;
     private String price;
-    private int imageResource; // Resource ID for the image
+    private String imageUrl; // Updated to hold image URL from Firestore
 
-    public PopularModel(String name, String rate, String price, int imageResource, String sold) {
+    public PopularModel(String name, String sold, String price, String imageUrl) {
         this.name = name;
-        this.rate = rate;
         this.price = price;
         this.sold = sold;
-        this.imageResource = imageResource;
+        this.imageUrl = imageUrl;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getRate() {
-        return rate;
-    }
-
     public String getPrice() {
         return price;
     }
 
-    public int getImageResource() {
-        return imageResource;
-    }
-
     public String getSold() {
         return sold;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
